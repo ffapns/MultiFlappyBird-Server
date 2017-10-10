5c71d0cdcec501a80769128324e24164dab2527a
Panusorn Srijamorn <fifa.pns@outlook.com>
2017-10-10 15:23:09 +0700

    remove dist

--- dist/api/players/controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import { Request, Response } from "express";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PlayerController {
-}
-exports.PlayerController = PlayerController;
--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ProductionConfig {
-    constructor() {
-        this.port = parseInt(process.env.PORT, 10) || 9001;
-    }
-}
-exports.ProductionConfig = ProductionConfig;
